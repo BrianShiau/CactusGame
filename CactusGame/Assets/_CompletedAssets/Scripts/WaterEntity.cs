a73e61b980491382854793d332eb7597e8edbecc
Michael <michaelevans4096@gmail.com>
2016-09-10 22:55:20 -0500

    added water entity!!!

---
+++ CactusGame/Assets/_CompletedAssets/Scripts/WaterEntity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class WaterEntity : MonoBehaviour {
+
+
+    public int water;
+	// Use this for initialization
+	void Start () {
+        water = Random.Range(25,50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().addWater(water);
+            Destroy(this.gameObject);
+        }
+    }
+}

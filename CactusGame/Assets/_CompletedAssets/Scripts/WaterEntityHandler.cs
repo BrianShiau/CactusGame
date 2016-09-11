7ee72909461d8aeff3ac0e1267579333006bd842
Michael <michaelevans4096@gmail.com>
2016-09-10 23:20:26 -0500

    added spawning to the water entities

---
+++ CactusGame/Assets/_CompletedAssets/Scripts/WaterEntityHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterEntityHandler : MonoBehaviour {
+
+    public GameObject waterEntity;
+    int count;
+    int delay = 1000;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 5, 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void Spawn()
+    {
+        Instantiate(waterEntity);
+    }
+}

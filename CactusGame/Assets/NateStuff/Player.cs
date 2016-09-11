f635985f7a73ee9e9ea3a4ad330d0391bae8c553
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-10 21:58:50 -0500

    Added UI interaction with player

--- CactusGame/Assets/NateStuff/Player.cs
+++ CactusGame/Assets/NateStuff/Player.cs
@@ -1,24 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
     public int health;
     public int water;
+    public int waterCap;
     public float respawnTimer;
     
     public List<Placeable> inventory;
-    public GameObject world; 
+    public GameObject world;
+    public GameObject UICanvas;
 
     private int selectedItem;
     private Vector3 respawnLocation;
     private float timeSinceDeath;
     private int startingHealth;
+    private Slider healthSlider;
+    private Slider waterSlider;
+    
 
     // Use this for initialization
     void Start () {
         respawnLocation = transform.position;
         startingHealth = health;
+        Slider[] UISliders = UICanvas.GetComponentsInChildren<Slider>();
+        healthSlider = UISliders[0];
+        waterSlider = UISliders[1];
+        healthSlider.maxValue = health;
+        waterSlider.maxValue = waterCap;
 
 	}
 	
@@ -61,6 +72,7 @@ public class Player : MonoBehaviour {
     public void takeDamage(int damage)
     {
         health-=damage;
+        healthSlider.value = health > 0 ? health : 0;
         if (health <= 0)
             kill();
     }
@@ -74,13 +86,20 @@ public class Player : MonoBehaviour {
 
     public void addWater(int added)
     {
-        water += added;
+        if (water + added <= waterCap)
+            water += added;
+        else
+            water = waterCap;
+
+        waterSlider.value = water;
+
     }
 
 
 
 	public void loseWater(int loss){
 		water -= loss;
+        waterSlider.value = water > 0 ? water : 0;
 		if (water <= 0){
 			//lose the game
 		}
